5946cdaf60a8c87c448dac70890fc549b3c415a2
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-05-31 20:14:52 -0300

    Implementado algumas melhorias para evitar possíveis Exceções

--- Program.cs
+++ Program.cs
@@ -9,46 +9,112 @@ namespace Unificador
     {
         static void Main(string[] args)
         {
-            List<string> lstArquivos = new List<string>();
             ConsoleColor corTextoOriginal = Console.ForegroundColor;
-
+            List<string> lstArquivos = new List<string>();
+            List<string> lstArquivosInseridos = new List<string>();
             Console.WriteLine(">---> Unificador de Arquivos <---<");
+
             Console.Write("\nDigite o local da pasta em que pretende trabalhar: ");
-
             Console.ForegroundColor = ConsoleColor.Green;
-            string pasta = Console.ReadLine();
+            string pasta;
+            while (ValidaLocalDoArquivo(pasta = Console.ReadLine()))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Você precisa inserir um caminho válido, tente novamente!");
+                Console.ForegroundColor = corTextoOriginal;
+                Console.Write("\nDigite o local da pasta em que pretende trabalhar: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
             Console.ForegroundColor = corTextoOriginal;
 
-            Console.Write("Quantos arquivos você pretende unificar? ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            int quantidadeDeArquivos = int.Parse(Console.ReadLine());
-            Console.ForegroundColor = corTextoOriginal;
-
+            int quantidadeDeArquivos = QuantidadeDeArquivos(corTextoOriginal);
             for (int i = 1; i <= quantidadeDeArquivos; i++)
             {
-                Console.Write($"\nDigite o nome do {i}º arquivo: ");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                string[] lines = File.ReadAllLines($@"{pasta}\{Console.ReadLine()}.csv");
-
-                foreach (string line in lines)
-                    lstArquivos.Add(line);
-
+                string[] lines = ValidaArquivo(lstArquivos, lstArquivosInseridos, pasta, i);
                 LogLinhasAdicionadas(corTextoOriginal, lines);
             }
             Console.WriteLine();
             Console.ForegroundColor = corTextoOriginal;
             EscreveArquivoUnificado(lstArquivos, pasta);
             FinalizaProcessamento(lstArquivos, corTextoOriginal);
-            
+
             Console.WriteLine();
             Console.ForegroundColor = corTextoOriginal;
             Console.Write("\nTecle enter para sair ");
             Console.ReadLine();
         }
 
+        private static bool ValidaLocalDoArquivo(string pasta)
+        {
+            return string.IsNullOrWhiteSpace(pasta);
+        }
+
+        private static int QuantidadeDeArquivos(ConsoleColor corTextoOriginal)
+        {
+            int quantidadeDeArquivos;
+            Console.Write("Quantos arquivos você pretende unificar? ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            while (!int.TryParse(Console.ReadLine(), out quantidadeDeArquivos))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nOpção inválida, tente novamente");
+                Console.ForegroundColor = corTextoOriginal;
+                Console.Write("Quantos arquivos você pretende unificar? ");
+            }
+            Console.ForegroundColor = corTextoOriginal;
+            return quantidadeDeArquivos;
+        }
+
+        private static string[] ValidaArquivo(List<string> lstArquivos, List<string> lstArquivosInseridos, string pasta, int i)
+        {
+            ConsoleColor corTextoOriginal = Console.ForegroundColor;
+            string[] arquivosParaUnificar = Directory.GetFiles(pasta);
+
+            string arquivo = VerificaSeArquivoJaFoiInserido(lstArquivosInseridos, i);
+            string arquivoCompleto = @$"{pasta}\{arquivo}.csv";
+
+            while (!arquivosParaUnificar.Contains(arquivoCompleto))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Arquivo {arquivo} não localizado, tente novamente");
+                Console.ForegroundColor = corTextoOriginal;
+                Console.Write($"\nDigite o nome do {i}º arquivo: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                arquivo = @$"{pasta}\{Console.ReadLine()}.csv";
+            }
+
+            string[] lines = File.ReadAllLines($@"{arquivoCompleto}");
+
+            foreach (string line in lines)
+                lstArquivos.Add(line);
+            lstArquivosInseridos.Add(arquivo);
+            return lines;
+        }
+
+        private static string VerificaSeArquivoJaFoiInserido(List<string> lstArquivosInseridos, int i)
+        {
+            ConsoleColor corTextoOriginal = Console.ForegroundColor;
+            Console.Write($"\nDigite o nome do {i}º arquivo: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string arquivo = Console.ReadLine();
+
+            while (lstArquivosInseridos.Contains(arquivo) && i > 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\tArquivo {arquivo} já foi inserido. Escolha outro!");
+                Console.ForegroundColor = corTextoOriginal;
+
+                Console.Write($"\nDigite o nome do {i}º arquivo: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                arquivo = Console.ReadLine();
+                Console.ForegroundColor = corTextoOriginal;
+            }
+            return arquivo;
+        }
+
         private static void LogLinhasAdicionadas(ConsoleColor corTextoOriginal, string[] lines)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"\tForam adicionados {lines.Count() - 1} linhas ao Arquivo Unificado");
             Console.ForegroundColor = corTextoOriginal;
         }
